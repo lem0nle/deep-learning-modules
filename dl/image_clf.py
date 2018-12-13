6f991414512627e76a5d59a96e44a2a797a017ec
Le Dai <daile96@qq.com>
2018-12-14 01:26:49 +0800

    Add dropout.

--- dl/image_clf.py
+++ dl/image_clf.py
@@ -1,36 +1,47 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import sys
 from torch.utils.data import DataLoader
 from sklearn.metrics import precision_recall_fscore_support
+from tqdm import tqdm
 
 
 class SimpleCNN(nn.Module):
-    def __init__(self, n_classes):
+    def __init__(self, n_classes, dropout_p=0.5):
         super().__init__()
         self.n_classes = n_classes
-        self.act = nn.LeakyReLU()
+        act = self.act = nn.ReLU()
+        dropout = self.dropout = nn.Dropout(dropout_p)
         self.feature = nn.Sequential(
-            # in_channels=3, out_channels=4, kernel_size=3x3
+            # in_channels=1, out_channels=4, kernel_size=3x3
+            dropout,
             nn.Conv2d(1, 4, 3),
-            self.act,
+            act,
+            dropout,
             nn.Conv2d(4, 8, 3),
-            self.act,
+            act,
             nn.MaxPool2d(2),
-            nn.Conv2d(8, 16, 5),
-            self.act,
-            nn.Conv2d(16, 32, 5),
-            self.act,
+            dropout,
+            nn.Conv2d(8, 16, 3),
+            act,
+            dropout,
+            nn.Conv2d(16, 32, 3),
+            act,
+            nn.MaxPool2d(2),
+            dropout,
+            nn.Conv2d(32, 64, 3),
+            act,
             nn.MaxPool2d(2)
         )
-        self.fc = nn.Linear(32, 32)
+        self.fc = nn.Linear(64, 32)
         self.out = nn.Linear(32, n_classes)
 
     def forward(self, x):
         x = self.feature(x)
         x = x.mean(dim=-1).mean(dim=-1)
-        x = self.act(self.fc(x))
-        return self.out(x)
+        x = self.act(self.fc(self.dropout(x)))
+        return self.out(self.dropout(x))
 
 
 class Trainer:
@@ -39,12 +50,17 @@ class Trainer:
         self.batch_size = batch_size
 
     def train(self, data, n_epochs=10):
-        # TODO: add validation dataset 
+        # train mode
+        self.model.train()
+        # TODO: add validation dataset
         loader = DataLoader(data, batch_size=self.batch_size, shuffle=True)
         optimizer = torch.optim.Adam(self.model.parameters())
 
         for i in range(n_epochs):
-            for i_batch, batch in enumerate(loader):
+            # tqdm for progress bar.
+            # Set file to avoid conflict with print in Jupyter.
+            for i_batch, batch in tqdm(enumerate(loader), total=len(loader),
+                                       file=sys.stdout):
                 y_pred = self.model(batch[0])
                 # no softmax
                 loss = F.cross_entropy(y_pred, batch[1])
@@ -59,15 +75,22 @@ class Trainer:
             print('epoch:', i, 'loss:', loss.mean().item())
 
     def evaluate(self, data):
+        # evaluation mode
+        self.model.eval()
         loader = DataLoader(data, batch_size=2 * self.batch_size)
-        predictions = []
-        groundtruth = []
-        for i_batch, batch in enumerate(loader):
-            y_pred = self.model(batch[0])
-            y_pred = y_pred.max(dim=1)[1]
-            y_true = batch[1]
-            predictions.append(y_pred)
-            groundtruth.append(y_true)
-        y_pred = torch.cat(predictions)
-        y_true = torch.cat(groundtruth)
-        print(precision_recall_fscore_support(y_true, y_pred, average='weighted'))
+
+        # predict result without computing grad
+        with torch.no_grad():
+            predictions = []
+            groundtruth = []
+            for i_batch, batch in tqdm(enumerate(loader), total=len(loader),
+                                       file=sys.stdout):
+                y_pred = self.model(batch[0])
+                y_pred = y_pred.max(dim=1)[1]
+                y_true = batch[1]
+                predictions.append(y_pred)
+                groundtruth.append(y_true)
+            y_pred = torch.cat(predictions)
+            y_true = torch.cat(groundtruth)
+        return precision_recall_fscore_support(
+            y_true, y_pred, average='weighted')
